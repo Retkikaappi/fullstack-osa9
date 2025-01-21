996fcd111b13fb820ac9274d984730747ef73726
Joonas Nuolikoski <joonas.nuolikoski@gmail.com>
2025-01-21 15:32:20 +0200

    added returns

--- ex-9-1-to-9-7/index.ts
+++ ex-9-1-to-9-7/index.ts
@@ -8,21 +8,26 @@ app.use(express.json());
 
 app.get('/hello', (_req, resp) => {
   resp.send('Hello Full Stack');
+  return;
 });
 
 app.get('/bmi', (req, resp) => {
   const { height, weight } = req.query;
   if (!height || !weight) {
     resp.status(400).json({ error: 'request is missing weight or height' });
+    return;
   }
   if (nanCheck(height) || nanCheck(weight)) {
     resp.status(400).json({ error: 'weight and height should be numbers' });
+    return;
   }
   if (Number(height) <= 0 || Number(weight) <= 0) {
     resp.status(400).json({ error: 'weight and height should be above 0' });
+    return;
   }
 
   resp.send(bmi(Number(height), Number(weight)));
+  return;
 });
 
 const PORT = 3001;
