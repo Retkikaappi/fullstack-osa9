dd371fc4a39d8bf6b2f3fa2cb43cfcd23a0278b6
Joonas Nuolikoski <joonas.nuolikoski@gmail.com>
2025-01-22 09:52:06 +0200

    production works, too

--- patientor/backend/build/src/index.js
+++ patientor/backend/build/src/index.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const diagnose_1 = __importDefault(require("./routes/diagnose"));
+const patient_1 = __importDefault(require("./routes/patient"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.get('/api/ping', (_req, resp) => {
@@ -12,6 +14,8 @@ app.get('/api/ping', (_req, resp) => {
     resp.send('pong');
     return;
 });
+app.use('/api/diagnoses', diagnose_1.default);
+app.use('/api/patients', patient_1.default);
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Running on http://localhost:${PORT}`);
